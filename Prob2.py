a7f3056d05664ec5f72cd7dca093d1a3ebb0854e
calvin <cd-public@users.noreply.github.com>
2024-09-12 17:26:44 -0700

    Initial commit

---
+++ Prob2.py
@@ -0,0 +1,46 @@
+#######################################
+# Name:
+# Collaborators:
+# Estimated time spent (hr):
+#######################################
+
+from english import ENGLISH_WORDS
+
+
+# Part A
+def contains_repeated_letters(word):
+    """Returns True if the word contains more than one instance of any letter.
+
+    Arguments:
+        word (str): The word to check
+    Returns:
+        (bool): True or False depending on if the word contains duplicated letters
+    """
+    # Your turn!
+
+
+
+
+
+# Part B
+def longest_no_repeats():
+    """Returns the longest word in the English dictionary without an repeated letters.
+
+    Arguments:
+        None
+    Returns:
+        (str): The longest word without repeated letters
+    """
+    # Your turn!
+
+
+
+
+
+if __name__ == '__main__':
+    # Part A
+    print(contains_repeated_letters("single"))
+    print(contains_repeated_letters("repeating"))
+
+    # Part B
+    # print(longest_no_repeats()) #Uncomment when you've finished Part B
